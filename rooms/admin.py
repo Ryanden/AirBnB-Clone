7e40a3ab58fc9ad25d7d206437843ffac18b2ed2
Ryanden <lockstom@gmail.com>
2021-01-08 00:07:05 +0900

    add Review and meta

--- rooms/admin.py
+++ rooms/admin.py
@@ -4,9 +4,23 @@ from . import models
 
 @admin.register(models.RoomType, models.Amenity, models.Facility, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
     pass
 
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+
+    """ Room Admin Definition """
+
+    pass
+
+
+@admin.register(models.Photo)
+class Phots(admin.ModelAdmin):
+
+    """ Room Admin Definition """
+
     pass
