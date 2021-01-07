dd15290911b51996a6e7410372935610a88b9874
Ryanden <lockstom@gmail.com>
2021-01-08 01:56:14 +0900

    Add Review

--- core/models.py
+++ core/models.py
@@ -8,6 +8,6 @@ class TimeStampModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-
     class Meta:
-      abstract = True
+        abstract = True
+
--- reviews/models.py
+++ reviews/models.py
@@ -16,3 +16,5 @@ class Review(core_models.TimeStampModel):
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
     room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.review} - {self.room}"
