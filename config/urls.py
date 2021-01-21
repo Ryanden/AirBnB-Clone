cb1c72723b645d91b38dce2137c354af56f33167
Ryanden <lockstom@gmail.com>
2021-01-22 00:34:24 +0900

    Add User Authenticate Login, Logout

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("core.urls", namespace="core")),
     path("conversations/", include("conversations.urls", namespace="conversations")),
+    path("users/", include("users.urls", namespace="users")),
     # path("lists/", include("lists.urls", namespace="lists")),
     # path("reservations/", include("reservations.urls", namespace="reservations")),
     # path("reviews/", include("reviews.urls", namespace="reviews")),
--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,4 @@ from rooms import views as room_views
 
 app_name = "core"
 
-urlpatterns = [path("", room_views.RoomView.as_view(), name="core")]
+urlpatterns = [path("", room_views.RoomView.as_view(), name="home")]
